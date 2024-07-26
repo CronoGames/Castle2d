41921c11732c121637ac5b977c38b2d028230912
Matt Brock <cronos1979@live.com>
2024-07-25 19:28:44 -0500

    Start template

---
+++ Source/CroHeader.h
@@ -0,0 +1,11 @@
+#ifndef CROHEADER_H
+#define CROHEADER_H
+/*
+ * © 2024 CronoGames. All rights reserved.
+ * Basic header files i use in most files
+ */
+
+#include "CroTypes.h"
+#include "CroMath.h"
+
+#endif
---
+++ Source/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * © 2024 CronoGames. All rights reserved.
+ */
+
+// All includes vare here
+#include "CroHeader.h"
+#include <iostream>
+
+// Main entry point for console programs
+int main()
+{
+    std::cout << PI << std::endl;
+    std::cout << sizeof(float) << std::endl;
+    std::cout << sizeof(float64) << std::endl;
+    std::cout << sizeof(float128) << std::endl;
+    return 0;
+}
